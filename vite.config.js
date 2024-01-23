b04202c92631ffa30f2dded055ae9b10906e4756
Maniprithvi <007maniprithvi@gmail.com>
2024-01-23 17:54:28 +0530

    deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/insta-2.0/'
+  base:'https://github.com/Maniprithvi/insta-2.0/'
   // "base":"/solardeploy"
 
 })
